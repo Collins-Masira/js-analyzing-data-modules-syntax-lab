aa5b22821b98891de7136916f957d6cf8a8722d4
Collins Masira Ondieki <collinsmasiraondieki@gmail.com>
2026-04-15 17:24:56 +0300

    Completed combineUsers lab

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+function combineUsers(...arrays) {
+    const mergedUsers = [].concat(...arrays);
 
+    const today = new Date();
+    const formattedDate = `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`;
 
+    return {
+        users: mergedUsers,
+        merge_date: formattedDate
+    };
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+    combineUsers
 };
